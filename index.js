48eab47809fd35d4b0da8506ceb94b894899f5a0
beerbumm <chelseaw.gfx@gmail.com>
2023-10-05 18:30:03 -0700

    added icon element and updated html icon

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function city(event) {
 }
 
 function showTemperature(response) {
-  console.log(response);
+  console.log(response.data);
+  let iconElement = document.querySelector("#icon");
   let windElement = document.querySelector("#wind-speed");
   let humidityElement = document.querySelector("#humidity");
   let temperature = Math.round(response.data.temperature.current);
@@ -23,6 +24,10 @@ function showTemperature(response) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = city;
 
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.temperature.humidity;
 }
